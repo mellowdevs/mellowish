3af3666340948355dcf57be843cf2e8314002290
MelAsAdaLovelace <kavcioglumelike@gmail.com>
2020-12-22 15:17:34 +0300

    app is ready

---
+++ api/src/test/java/com/mellowdevs/mellowish/MellowishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mellowdevs.mellowish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MellowishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
